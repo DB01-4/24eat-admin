fec36cbd4b1114fdc0d829c47d43eda41809c90d
Jovi Simons <jovisimons009@gmail.com>
2021-10-12 14:31:50 +0200

    added pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Category from './Pages/Category';
+import Product from './Pages/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/category' component={Category} />
+              <Route exact path='/product' component={Product}/>
+          </Switch>
+        </div>
+      </Router>
   );
 }
+
+//Home page from here 
+
+const Home = () => (
+
+  <div>
+    <h1>Home</h1>
+
+  </div>
+   
+);
 
 export default App;
---
+++ src/Pages/Category.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Category extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Category</h1>
+            </div>
+        )
+    }
+}
