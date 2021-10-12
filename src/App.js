8334f09aa6f74010fdf700cf9541f06e79a24603
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2021-10-12 15:49:00 +0200

    added first components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Category from './Pages/Category';
 import Product from './Pages/Product';
+import Edit from './components/Edit';
 
 function App() {
   return (
     <Router>
         <div className="App">
           <Switch>
+            <Edit/>
               <Route exact path='/' component={Home} />
               <Route exact path='/category' component={Category} />
               <Route exact path='/product' component={Product}/>
