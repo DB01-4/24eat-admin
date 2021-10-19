ee8fb9b3ae9ff76ea3835234d492a9337ee1776c
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2021-10-19 10:57:48 +0200

    working version for adding dishes without layout

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import './App.css';
 import React from "react";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Category from './Pages/Category';
-import Product from './Pages/Product';
-import Edit from './components/Edit';
+import Dish from './Pages/Dish';
 
 function App() {
   return (
     <Router>
         <div className="App">
           <Switch>
-            <Edit/>
               <Route exact path='/' component={Home} />
               <Route exact path='/category' component={Category} />
-              <Route exact path='/product' component={Product}/>
+              <Route exact path='/product' component={Dish}/>
           </Switch>
         </div>
       </Router>
