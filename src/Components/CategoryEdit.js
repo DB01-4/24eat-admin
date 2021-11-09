2d04fc4978650973fb07014404f908e079894009
Jovi Simons <jovisimons009@gmail.com>
2021-11-09 15:04:33 +0100

    edit working

--- src/Components/CategoryEdit.js
+++ src/Components/CategoryEdit.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { useEffect, useState} from "react";
 import PropTypes from 'prop-types';
-import { useForm } from "react-hook-form";
 import axios from "axios";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import {Dialog, DialogActions, DialogTitle, Button, TextField} from '@mui/material';
 
 
 export default function CategoryEdit(props) {
@@ -19,33 +14,38 @@ export default function CategoryEdit(props) {
 }
 
   const { onClose, selectedCard, open, url} = props;
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const [value, setValue] = useState(initialFValues);
+  const [values, setValues] = useState(initialFValues);
 
   const handleClose = () => {
     onClose();
   };
 
   const onChange = e => {
-    const { name, values } = e.target
-    console.log(e.target)
-    setValue({...value,[name]: values})
+    const { name, value } = e.target
+    setValues({
+      ...values,
+      [name]: value
+  })
+    console.log(values)
 }
 
   useEffect(() => {
+    console.log("test")
     if (selectedCard != null)
-        setValue({...selectedCard})
-        console.log(value)
+        setValues({...selectedCard})
 }, [selectedCard])
 
-  const onSubmit = data => 
-  axios.put(url+selectedCard.id, data)
+  const handleSubmit = e => {
+  axios.put(url+selectedCard.id, values)
     .then(function (response) {
       console.log(response);
     })
     .catch(function (error) {
-      console.log(error);
+     console.log(error);
   });
+  window.location.reload(false);
+  handleClose();
+}
 
   return (
     <Dialog onClose={handleClose} open={open}>
@@ -53,40 +53,42 @@ export default function CategoryEdit(props) {
         <form>
           <TextField
            id="outlined-multiline-flexible"
-           label="Multiline"
+           label="name"
+           name="name"
            multiline
            maxRows={4}
-           value={value.name}
+           defaultValue={values.name}
            onChange={onChange}
-           {...register("name", { required: true })} 
           />
-            {errors.exampleRequired && <p>This field is required</p>}
+
+
 
             <TextField
            id="outlined-multiline-flexible"
-           label="Multiline"
+           label="description"
+           name="description"
            multiline
            maxRows={4}
-           value={value.description}
+           defaultValue={values.description}
            onChange={onChange}
-           {...register("description", { required: true })} 
           />
-            {errors.exampleRequired && <p>This field is required</p>}
+
+
 
             <TextField
            id="outlined-multiline-flexible"
-           label="Multiline"
+           label="image"
+           name="image"
            multiline
            maxRows={4}
-           value={value.description}
+           defaultValue={values.description}
            onChange={onChange}
-           {...register("image", { required: true })} 
           />
-            {errors.exampleRequired && <p>This field is required</p>}
+
         </form>
         <DialogActions>
           <Button onClick={handleClose}>Close</Button>
-          <Button onClick={handleSubmit(onSubmit)} autoFocus>Submit</Button>
+          <Button onClick={handleSubmit} autoFocus>Submit</Button>
         </DialogActions>
     </Dialog>
   );
--- src/Pages/Category.js
+++ src/Pages/Category.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import useFetch from "../API/useFetch";
-import CategoryList from '../Components/CategoryList';
+import CategoryList from '../components/CategoryList';
 
 export default function Category(){
 
