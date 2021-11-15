517d40b18e5582ba26cf1510143d4ff948a0c3a6
Semm6 <sem.dewilde6@gmail.com>
2021-11-15 10:22:31 +0100

    Styled edit popupp

--- src/Components/CategoryEdit.js
+++ src/Components/CategoryEdit.js
@@ -1,83 +1,99 @@
 import React from "react";
 import { useEffect, useState} from "react";
+import CategorieEdit from "../Style/categorieEdit.css";
 import PropTypes from 'prop-types';
-import { useForm } from "react-hook-form";
 import axios from "axios";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import {Dialog, DialogActions, DialogTitle, Button, TextField} from '@mui/material';
 
 
 export default function CategoryEdit(props) {
 
   const initialFValues = {
     name: '',
-    description: '',
+    description: null,
     image: ''
 }
 
   const { onClose, selectedCard, open, url} = props;
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const [value, setValue] = useState(initialFValues);
+  const [values, setValues] = useState(initialFValues);
 
   const handleClose = () => {
     onClose();
   };
 
+  const onChange = e => {
+    const { name, value } = e.target
+    setValues({
+      ...values,
+      [name]: value
+  })
+    console.log(values)
+}
+
   useEffect(() => {
+    console.log("test")
     if (selectedCard != null)
-        setValue({...selectedCard})
-        console.log(value)
+        setValues({...selectedCard})
 }, [selectedCard])
 
-  const onSubmit = data => 
-  axios.put(url+selectedCard.id, data)
+  const handleSubmit = e => {
+  axios.put(url+selectedCard.id, values)
     .then(function (response) {
       console.log(response);
     })
     .catch(function (error) {
-      console.log(error);
+     console.log(error);
   });
+  window.location.reload(false);
+  handleClose();
+}
 
   return (
     <Dialog onClose={handleClose} open={open}>
-        <DialogTitle>Edit</DialogTitle>
-        <form>
+        <DialogTitle className="editheader">Edit</DialogTitle>
+        <form className="form">
+          <div className="textfield">
           <TextField
            id="outlined-multiline-flexible"
-           label="Multiline"
+           label="name"
+           name="name"
            multiline
            maxRows={4}
-           value={value.name}
-           {...register("name", { required: true })} 
+           defaultValue={values.name}
+           onChange={onChange}
           />
-            {errors.exampleRequired && <p>This field is required</p>}
+          </div>
 
+
+          <div className="textfield">
             <TextField
            id="outlined-multiline-flexible"
+           label="description"
+           name="description"
            multiline
            maxRows={4}
-           value={value.description}
-           {...register("description", { required: true })} 
+           defaultValue={values.description}
+           onChange={onChange}
           />
-            {errors.exampleRequired && <p>This field is required</p>}
+          </div>
 
+
+          <div className="textfield">
             <TextField
            id="outlined-multiline-flexible"
-           label="Multiline"
+           label="image"
+           name="image"
            multiline
            maxRows={4}
-           value={value.description}
-           {...register("image", { required: true })} 
+           defaultValue={values.description}
+           onChange={onChange}
           />
-            {errors.exampleRequired && <p>This field is required</p>}
+          </div>
+
         </form>
         <DialogActions>
           <Button onClick={handleClose}>Close</Button>
-          <Button onClick={handleSubmit(onSubmit)} autoFocus>Submit</Button>
-          <Button onClick={() => console.log('aids')}>Get aids in console</Button>
+          <Button onClick={handleSubmit} autoFocus>Submit</Button>
         </DialogActions>
     </Dialog>
   );
