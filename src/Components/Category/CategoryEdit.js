34d0bc07a228c05f0de6aec0b865854ad7befbb7
Semm6 <sem.dewilde6@gmail.com>
2021-12-13 13:23:25 +0100

    CRUD protected API requests with JWT

--- src/Components/Category/CategoryEdit.js
+++ src/Components/Category/CategoryEdit.js
@@ -1,102 +1,109 @@
 import React from "react";
-import { useEffect, useState} from "react";
-import PropTypes from 'prop-types';
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
-import {Dialog, DialogActions, DialogTitle, Button, TextField} from '@mui/material';
-
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  Button,
+  TextField,
+} from "@mui/material";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function CategoryEdit(props) {
-
   const initialFValues = {
-    name: '',
+    name: "",
     description: null,
-    image: ''
-}
+    image: "",
+  };
 
-  const { onClose, selectedCard, open, url} = props;
+  const { onClose, selectedCard, open, url } = props;
   const [values, setValues] = useState(initialFValues);
+  const categoryUrl = "http://localhost:8080";
+  const { getAccessTokenSilently } = useAuth0();
 
   const handleClose = () => {
     onClose();
   };
 
-  const onChange = e => {
-    const { name, value } = e.target
+  const onChange = (e) => {
+    const { name, value } = e.target;
     setValues({
       ...values,
-      [name]: value
-  })
-    console.log(values)
-}
+      [name]: value,
+    });
+    console.log(values);
+  };
 
   useEffect(() => {
-    console.log("test")
-    if (selectedCard != null)
-        setValues({...selectedCard})
-}, [selectedCard])
+    if (selectedCard != null) setValues({ ...selectedCard });
+  }, [selectedCard]);
 
-  const handleSubmit = e => {
-  axios.put(url+selectedCard.id, values)
-    .then(function (response) {
-      console.log(response);
-      window.location.reload(false);
-    })
-    .catch(function (error) {
-     console.log(error);
-  });
-  handleClose();
-}
-
+  const handleSubmit = async (e) => {
+    const token = await getAccessTokenSilently();
+    axios
+      .put(`${categoryUrl}/api/private/categories/` + selectedCard.id, values, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        window.location.reload(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    handleClose();
+  };
 
   return (
     <Dialog onClose={handleClose} open={open}>
-        <DialogTitle className="editheader">Edit</DialogTitle>
-        <form className="form">
-          <div className="textfield">
+      <DialogTitle className="editheader">Edit</DialogTitle>
+      <form className="form">
+        <div className="textfield">
           <TextField
-           id="outlined-multiline-flexible"
-           label="name"
-           name="name"
-           multiline
-           maxRows={4}
-           defaultValue={values.name}
-           onChange={onChange}
+            id="outlined-multiline-flexible"
+            label="name"
+            name="name"
+            multiline
+            maxRows={4}
+            defaultValue={values.name}
+            onChange={onChange}
           />
-          </div>
+        </div>
 
-
-          <div className="textfield">
-            <TextField
-           id="outlined-multiline-flexible"
-           label="description"
-           name="description"
-           multiline
-           maxRows={4}
-           defaultValue={values.description}
-           onChange={onChange}
+        <div className="textfield">
+          <TextField
+            id="outlined-multiline-flexible"
+            label="description"
+            name="description"
+            multiline
+            maxRows={4}
+            defaultValue={values.description}
+            onChange={onChange}
           />
-          </div>
+        </div>
 
-
-
-          <div className="textfield">
-            <TextField
-           id="outlined-multiline-flexible"
-           label="image"
-           name="image"
-           multiline
-           maxRows={4}
-           defaultValue={values.description}
-           onChange={onChange}
+        <div className="textfield">
+          <TextField
+            id="outlined-multiline-flexible"
+            label="image"
+            name="image"
+            multiline
+            maxRows={4}
+            defaultValue={values.description}
+            onChange={onChange}
           />
-          </div>
-
-
-        </form>
-        <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-          <Button onClick={handleSubmit} autoFocus>Submit</Button>
-        </DialogActions>
+        </div>
+      </form>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+        <Button onClick={handleSubmit} autoFocus>
+          Submit
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
--- src/Pages/Category.js
+++ src/Pages/Category.js
@@ -3,48 +3,14 @@ import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "../Components/Login/Loading";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
+import CategoryList from "../Components/Category/CategoryList";
 
 const Category = () => {
-  const serverUrl = "http://localhost:8080";
-
-  const { getAccessTokenSilently, isLoading, user } = useAuth0();
-
-  const [categories, setCategories] = useState([]);
-
-  // Fetch categories
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        //get token access token to make request
-        const token = await getAccessTokenSilently();
-
-        console.log(token);
-
-        //private endpoint
-        axios
-          .get(`${serverUrl}/api/private/products`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          })
-          .then((categories) => {
-            setCategories(categories.data);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    if (isLoading === false) {
-      fetchCategories();
-    }
-  }, [isLoading]);
-
-  //map all the names of categories
-  return categories.map((category, index) => {
-    return <div>{category.name}</div>;
-  });
+  return <CategoryList />;
 };
 
-export default withAuthenticationRequired(Category, {
-  onRedirecting: () => <Loading />,
-});
+// export default withAuthenticationRequired(Category, {
+//   onRedirecting: () => <Loading />,
+// });
+
+export default Category;
